27521712e422fa44be68624c5af8ba5fc62821e7
Mazen Saeed <104124933+Imazen7@users.noreply.github.com>
2025-09-01 22:32:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
       usePolling: true
     },
-    host: true, // so it listens on 0.0.0.0
+    host: true, 
     strictPort: true
   }
 })
