156374aebeb5aeb99df0176134c528e0b46ec0d0
Mazen Saeed <104124933+Imazen7@users.noreply.github.com>
2025-09-02 11:19:33 +0530

    Add files via upload

---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBz9fotxeWmwQRaMhVe-NkpQb9fvnm0mu0",
+  authDomain: "hostel-hub-824eb.firebaseapp.com",
+  projectId: "hostel-hub-824eb",
+  storageBucket: "hostel-hub-824eb.firebasestorage.app",
+  messagingSenderId: "49992358090",
+  appId: "1:49992358090:web:6a14c3063b4121b21ffc33",
+  measurementId: "G-7EEFJB9C58"
+};
+
+export default firebaseConfig;
